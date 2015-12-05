1e8eff787f522014e56fc723596a563c5cccc4c0
litaojun <78069455@qq.com>
2015-12-05 14:50:11 +0800

    dddd

--- package/chge/ChargeMgr.py
+++ package/chge/ChargeMgr.py
@@ -13,7 +13,7 @@ sys.setdefaultencoding('utf8')
 class ChargeMgr(AddSim):
     def __init__(self):
         SelenuieWeb.__init__(self)
-        self.addlayout = elementTabLayout(u'E:\kankan\VaCache\页面布局\套餐管理\套餐资费信息.xlsx',u'新增资费')
+        self.addlayout = elementTabLayout(u'E:\kankan\VaCache\椤甸潰甯冨眬\濂楅绠＄悊\濂楅璧勮垂淇℃伅.xlsx',u'鏂板璧勮垂')
         self.addlayout.initElementData()
     def enter_NewCharge_Win(self):
         #addbut = self.browser.find_element_by_css_selector("div[class=\" x-panel x-border-panel\"] div[class=\" x-portal-column x-column\"]>div[class=\" x-panel x-portlet\"]>div.x-panel-bwrap div[class=\"x-panel-tbar x-panel-tbar-noheader\"] button")
@@ -63,14 +63,14 @@ class ChargeMgr(AddSim):
 if __name__ == "__main__":
     #charge = ChargeMgr()
     a=ChargeMgr()
-    cgritem=["11",'CNY','小时','ssssssssssss']
+    cgritem=["11",'CNY','灏忔椂','ssssssssssss']
     a.login_Skyroam_Web("admin","111111")
     print "start-----------------------"
-    a.select_Menu_Item("套餐管理","资费管理")
+    a.select_Menu_Item("濂楅绠＄悊","璧勮垂绠＄悊")
     a.enter_NewCharge_Win()
-    a.enter_One_SimItem(u'价格','120')
-    a.enter_One_SimItem(u'货币类型','HKD')
-    a.enter_One_SimItem(u'单位',u'小时')
+    a.enter_One_SimItem(u'浠锋牸','120')
+    a.enter_One_SimItem(u'璐у竵绫诲瀷','HKD')
+    a.enter_One_SimItem(u'鍗曚綅',u'灏忔椂')
     a.click_Save()
     #a.input_Charge_Item_Sv(cgritem)
     a.check_Result("operated succesfully!")
